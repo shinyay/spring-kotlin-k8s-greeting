32e1ff109972c3727cba0adb8c6816609b9d36d5
shinyay <shinya.com@gmail.com>
2019-05-15 23:32:10 +0900

    Initial Commit

---
+++ src/main/kotlin/io/pivotal/shinyay/simple/HelloApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal.shinyay.simple
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloApplication
+
+fun main(args: Array<String>) {
+    runApplication<HelloApplication>(*args)
+}
